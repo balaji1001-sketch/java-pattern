59e9f12d3cd7b60a5c60607a8a447349deb5e021
balaji1001-sketch <balajijayakumar00031@gmail.com>
2026-01-28 12:50:09 +0530

    Add new files and changes

---
+++ SquarePattern.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class SquarePattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the square: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {      // Rows
+            for (int j = 1; j <= n; j++) {  // Columns
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
